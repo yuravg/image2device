0098aaebbc431a5eb0f14925cbebe7baeedbe9ed
Yuriy VG <yuravg@gmail.com>
2023-05-05 14:11:46 +0300

    Add support bmaptool
    Now this wrapper support bmaptool to use as the command to write block
    devices.

--- image2device.sh
+++ image2device.sh
@@ -2,20 +2,21 @@
 
 # Author: Yuriy Gritsenko
 # URL: https://github.com/yuravg/image2device
-# Time-stamp: <2023-05-05 12:57:55>
+# Time-stamp: <2023-05-05 17:24:51>
 # License: MIT License. If not, see <https://www.opensource.org/licenses/MIT>.
 
 #
 # Script to copy a image-file to a block device(SD card, Flash drive, etc.),
-# it is just a wrapper for "dd" tool.
+# it is just a wrapper for the "dd" and "bmaptool" tools.
 #
 # Usage
 #  <script_name> <path-to-image> <path-to-device>
 #
 # Example
 #  image2device.sh ./sdimage.img /dev/sdc
+#  image2device.sh -b ./sdimage.img /dev/sdc
 
-SCRIPT_VERSION="0.2.1"
+SCRIPT_VERSION="1.0.0"
 
 echo "+----------------------------------------------------------------------+"
 echo "| Copy image file to block device                                      |"
@@ -28,9 +29,10 @@ if [ "$1" = '-V' ] || [ "$1" = '--version' ]; then
     exit 0
 fi
 
-if [ "$#" -ne 2 ] || [ "$1" = '-h' ] || [ "$1" = '--help' ] || [ "$1" = '-help' ]; then
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ] \
+       || [ "$1" = '-h' ] || [ "$1" = '--help' ] || [ "$1" = '-help' ]; then
     echo "Usage:"
-    echo "  $SCRIPT_NAME [image] [device]"
+    echo "  $SCRIPT_NAME [-b] [image] [device]"
     echo "               [--help | -h] [--version | -V]"
     echo "ARGS:"
     echo "    <image>"
@@ -38,12 +40,15 @@ if [ "$#" -ne 2 ] || [ "$1" = '-h' ] || [ "$1" = '--help' ] || [ "$1" = '-help' 
     echo "    <device>"
     echo "            Path to block device."
     echo "OPTIONS:"
+    echo "    -b"
+    echo "            Use 'bmaptool' to write image (by default: 'dd')."
     echo "    --version | -V"
     echo "            Prints version information."
     echo "    --help | -h"
     echo "            Prints help information."
     echo "Usage example:"
     echo "  $SCRIPT_NAME ./sdimage.img /dev/sdc"
+    echo "  $SCRIPT_NAME -b ./sdimage.img /dev/sdc"
     exit 0
 fi
 
@@ -76,19 +81,29 @@ red_echo (){
     printf "\\e[31m\\e[1m%s\\e[0m\\n" "$1"
 }
 
-if [ ! -s "$1" ]; then
-    red_echo "ERROR! Can't find image file(or zero size): '$1'!"
+if [ "$1" = "-b" ]; then
+    ARG_BMAP_EN=1
+    ARG_IMG="$2"
+    ARG_DEV="$3"
+else
+    ARG_BMAP_EN=0
+    ARG_IMG="$1"
+    ARG_DEV="$2"
+fi
+
+if [ ! -s "$ARG_IMG" ]; then
+    red_echo "ERROR! Can't find image file(or zero size): '$ARG_IMG'!"
     exit 2
 else
-    IMAGE_FILE="$1"
+    IMAGE_FILE="$ARG_IMG"
     IMAGE_FILE_SIZE=$(du -h "$IMAGE_FILE" | awk '{print $1}')
 fi
 
-if [ ! -b "$2" ]; then
-    red_echo "ERROR! Can't find block device: '$2'"
+if [ ! -b "$ARG_DEV" ]; then
+    red_echo "ERROR! Can't find block device: '$ARG_DEV'"
     exit 3
 else
-    DEVICE="$2"
+    DEVICE="$ARG_DEV"
 fi
 
 echo ""
@@ -133,22 +148,55 @@ if mount | grep -c "$DEVICE" &>/dev/null; then
     fi
 fi
 
-CMD="sudo dd bs=32M conv=sync status=progress if=$IMAGE_FILE of=$DEVICE"
-underline_echo "Command to execute:"
-echo "$CMD"
-echo ""
+run_cmd () {
+    underline_echo "Command to execute:"
+    echo "$CMD"
+    echo ""
 
-echo "WARNING! All data on the device ($DEVICE) will be erased!"
-if ! fun_yesno "Would you like begin writing" ; then
-    echo "Exit without image recording!"
-    exit 1
-fi
+    echo "WARNING! All data on the device ($DEVICE) will be erased!"
+    if ! fun_yesno "Would you like begin writing" ; then
+        echo "Exit without image recording!"
+        exit 1
+    fi
 
-echo "Writing ..."
-if ! $CMD; then
-    red_echo "Error! Unable to write data."
+    echo "Writing ..."
+    if ! $CMD; then
+        red_echo "Error! Unable to write data."
+    else
+        green_echo "Done."
+    fi
+}
+
+dd_write () {
+    CMD="sudo dd bs=32M conv=sync status=progress if=$IMAGE_FILE of=$DEVICE"
+    run_cmd
+}
+
+btool_write() {
+    BMAP_FNAME="${IMAGE_FILE%.*}-${IMAGE_FILE##*.}.bmap"
+
+    CMD="bmaptool create -o $BMAP_FNAME $IMAGE_FILE"
+
+    if [ -s "$BMAP_FNAME" ]; then
+        BMAP_FTIME=$(date '+%Y-%m-%d %H:%M:%S' -r "$BMAP_FNAME")
+        echo "Note: existing bmap-file: $BMAP_FNAME ($BMAP_FTIME),"
+        echo "      will be used to write the image."
+    else
+        echo "Create bmap-file, run:"
+        echo "$CMD"
+        if ! $CMD; then
+            red_echo "Error! Can not create bmap-file: $BMAP_FNAME!"
+        fi
+    fi
+
+    CMD="sudo bmaptool copy --bmap $BMAP_FNAME $IMAGE_FILE $DEVICE"
+    run_cmd
+}
+
+if [ "$ARG_BMAP_EN" -eq 1 ]; then
+    btool_write
 else
-    green_echo "Done."
+    dd_write
 fi
 
 # This is for the sake of Emacs.
